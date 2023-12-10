c47ba425d784d0168cf61cb47b7eb0afe4b75916
Farhanalisayed <S.farhan.ali.1996@gmail.com>
2023-12-10 20:11:02 +0000

    Sol

---
+++ src/components/Products/index.js
@@ -0,0 +1,14 @@
+// Write your JS code here
+import './index.css'
+
+const Product = () => (
+  <div className="not-found-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+      alt="product"
+      className="not-found-img"
+    />
+  </div>
+)
+
+export default Products
