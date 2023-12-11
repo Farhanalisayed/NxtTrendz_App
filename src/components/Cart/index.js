e389d2e0c264c822f2825e4c556ad6b21b8588fb
Farhanalisayed <151043508+Farhanalisayed@users.noreply.github.com>
2023-12-11 17:34:41 +0530

    Update index.js

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,14 +1,24 @@
-// Write your JS code here
+import Cookies from 'js-cookie'
+import {Redirect} from 'react-router-dom'
+import Header from '../Header'
 import './index.css'
 
-const Cart = () => (
-  <div className="not-found-container">
-    <img
-      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
-      alt="cart"
-      className="not-found-img"
-    />
-  </div>
-)
-
+const Cart = () => {
+  const jwtToken = Cookies.get('jwt_token')
+  if (jwtToken === undefined) {
+    return <Redirect to="login" />
+  }
+  return (
+    <>
+      <Header />
+      <div className="not-found-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+          alt="cart"
+          className="not-found-img"
+        />
+      </div>
+    </>
+  )
+}
 export default Cart
